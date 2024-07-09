e7b93977e2ea04741b130d6970de3e7b3a5be474
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-07-09 05:48:04 -0700

    add holesky deployment

--- script/bin/genesis.rs
+++ script/bin/genesis.rs
@@ -36,7 +36,7 @@ pub async fn main() {
     if let Some(block) = args.block {
         let header_hash = data_fetcher.fetch_header_hash(block).await;
         info!(
-            "\nGENESIS_HEIGHT={:?}\nGENESIS_HEADER={}\nBLOBSTREAM_X_PROGRAM_VKEY={}\n",
+            "\nGENESIS_HEIGHT={:?}\nGENESIS_HEADER={}\nSP1_BLOBSTREAM_PROGRAM_VKEY={}\n",
             block,
             header_hash.to_string(),
             vk.bytes32(),
@@ -46,7 +46,7 @@ pub async fn main() {
         let header_hash = data_fetcher.fetch_header_hash(latest_block_height).await;
 
         info!(
-            "\nGENESIS_HEIGHT={:?}\nGENESIS_HEADER={}\nBLOBSTREAM_X_PROGRAM_VKEY={}\n",
+            "\nGENESIS_HEIGHT={:?}\nGENESIS_HEADER={}\nSP1_BLOBSTREAM_PROGRAM_VKEY={}\n",
             latest_block_height,
             header_hash.to_string(),
             vk.bytes32(),
