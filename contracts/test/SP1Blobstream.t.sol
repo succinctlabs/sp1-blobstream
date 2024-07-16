fab3b268ae393887f5d8f528dd11238c8af9d463
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-07-16 15:35:42 -0700

    feat: add approved relayer to contract

--- contracts/test/SP1Blobstream.t.sol
+++ contracts/test/SP1Blobstream.t.sol
@@ -19,7 +19,7 @@ contract SP1BlobstreamTest is Test {
         require(keccak256(encodedInput) == keccak256(packedEncodedInput), "packed matches");
     }
 
-    function testGetEncodePackedNextHeader() public view {
+    function testGetEncodePackedNextHeader() public pure {
         // http://64.227.18.169:26657/block?height=10000
         uint64 height = 10000;
         bytes32 header = hex"A0123D5E4B8B8888A61F931EE2252D83568B97C223E0ECA9795B29B8BD8CBA2D";
@@ -27,7 +27,7 @@ contract SP1BlobstreamTest is Test {
         console.logBytes(encodedInput);
     }
 
-    function testGetEncodePackedHeaderRange() public view {
+    function testGetEncodePackedHeaderRange() public pure {
         // http://64.227.18.169:26657/block?height=10000
         uint64 height = 10000;
         bytes32 header = hex"A0123D5E4B8B8888A61F931EE2252D83568B97C223E0ECA9795B29B8BD8CBA2D";
