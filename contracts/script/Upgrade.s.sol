bf7bd128f080b0873192ffdf65678c63aa490db3
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-07-17 13:35:57 -0700

    fix

--- contracts/script/Upgrade.s.sol
+++ contracts/script/Upgrade.s.sol
@@ -6,6 +6,7 @@ import {SP1Blobstream} from "../src/SP1Blobstream.sol";
 import {ERC1967Proxy} from "@openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
 import {SP1MockVerifier} from "@sp1-contracts/SP1MockVerifier.sol";
 import {ISP1Verifier} from "@sp1-contracts/ISP1Verifier.sol";
+import {BaseScript} from "./Base.s.sol";
 
 // Required environment variables:
 // - CONTRACT_ADDRESS
@@ -25,16 +26,16 @@ contract UpgradeScript is BaseScript {
             string.concat("CONTRACT_ADDRESS_", vm.toString(block.chainid));
         address existingProxyAddress = vm.envAddress(contractAddressKey);
 
-        SP1Vector sp1Vector = SP1Vector(address(existingProxyAddress));
+        SP1Blobstream sp1Blobstream = SP1Blobstream(address(existingProxyAddress));
 
         // // Update the SP1 Verifier address and the program vkey.
         // if (vm.envBool("MOCK")) {
         //     SP1MockVerifier mockVerifier = new SP1MockVerifier();
-        //     sp1Vector.updateVerifier(address(mockVerifier));
+        //     sp1Blobstream.updateVerifier(address(mockVerifier));
         // } else {
-        //     sp1Vector.updateVerifier(vm.envAddress("SP1_VERIFIER_ADDRESS"));
+        //     sp1Blobstream.updateVerifier(vm.envAddress("SP1_VERIFIER_ADDRESS"));
         // }
 
-        sp1Vector.updateVectorXProgramVkey(vm.envBytes32("SP1_VECTOR_PROGRAM_VKEY"));
+        sp1Blobstream.updateProgramVkey(vm.envBytes32("SP1_BLOBSTREAM_PROGRAM_VKEY"));
     }
 }
