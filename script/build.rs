0caf53f5b1a1ec9b010e3ae49edf97f9021fdc83
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-08-05 18:10:32 -0700

    upgrade to 1.1.0

--- script/build.rs
+++ script/build.rs
@@ -1,11 +1,12 @@
 use sp1_helper::{build_program_with_args, BuildArgs};
 
 fn main() {
-    build_program_with_args(
-        "../program",
-        BuildArgs {
-            ignore_rust_version: true,
-            ..Default::default()
-        },
-    )
+    // TODO: Move to Docker builds in the future.
+    // build_program_with_args(
+    //     "../program",
+    //     BuildArgs {
+    //         ignore_rust_version: true,
+    //         ..Default::default()
+    //     },
+    // )
 }
