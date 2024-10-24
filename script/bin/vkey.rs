930c2b7792795cc1fd3e231c8decde44d8ec85c8
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-10-24 10:52:41 -0700

    3.0.0

---
+++ script/bin/vkey.rs
@@ -0,0 +1,17 @@
+//! To build the binary:
+//!
+//!     `cargo build --release --bin genesis`
+//!
+//!
+//!
+//!
+//!
+use sp1_sdk::{HashableKey, ProverClient};
+const BLOBSTREAMX_ELF: &[u8] = include_bytes!("../../elf/blobstream-elf");
+
+#[tokio::main]
+pub async fn main() {
+    let client = ProverClient::new();
+    let (_pk, vk) = client.setup(BLOBSTREAMX_ELF);
+    println!("VK: {}", vk.bytes32());
+}
