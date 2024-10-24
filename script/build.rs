a779368d21213a279ee657350b08113a644b2406
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-10-24 12:32:33 -0700

    turn off build

--- script/build.rs
+++ script/build.rs
@@ -2,13 +2,12 @@
 use sp1_build::{build_program_with_args, BuildArgs};
 
 fn main() {
-    // Note: Comment this in to build the program.
-    build_program_with_args(
-        "../program",
-        BuildArgs {
-            docker: true,
-            elf_name: "blobstream-elf".to_string(),
-            ..Default::default()
-        },
-    )
+    // build_program_with_args(
+    //     "../program",
+    //     BuildArgs {
+    //         docker: true,
+    //         elf_name: "blobstream-elf".to_string(),
+    //         ..Default::default()
+    //     },
+    // )
 }
