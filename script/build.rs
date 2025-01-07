a7bd0463b7daa59a4b25af242eb3e5d298384987
Ratan Kaliani <ratankaliani@berkeley.edu>
2025-01-06 20:23:28 -0800

    wip

--- script/build.rs
+++ script/build.rs
@@ -2,13 +2,12 @@
 use sp1_build::{build_program_with_args, BuildArgs};
 
 fn main() {
-    // build_program_with_args(
-    //     "../program",
-    //     BuildArgs {
-    //         docker: true,
-    //         tag: "v3.0.0".to_string(),
-    //         elf_name: "blobstream-elf".to_string(),
-    //         ..Default::default()
-    //     },
-    // )
+    build_program_with_args(
+        "../program",
+        BuildArgs {
+            docker: true,
+            elf_name: "blobstream-elf".to_string(),
+            ..Default::default()
+        },
+    )
 }
