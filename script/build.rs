db160e656967e71705636e096087bc1d91201d5c
Ratan Kaliani <ratankaliani@berkeley.edu>
2025-01-06 21:59:07 -0800

    fix

--- script/build.rs
+++ script/build.rs
@@ -2,12 +2,13 @@
 use sp1_build::{build_program_with_args, BuildArgs};
 
 fn main() {
-    build_program_with_args(
-        "../program",
-        BuildArgs {
-            docker: true,
-            elf_name: "blobstream-elf".to_string(),
-            ..Default::default()
-        },
-    )
+    // build_program_with_args(
+    //     "../program",
+    //     BuildArgs {
+    //         docker: true,
+    //         elf_name: "blobstream-elf".to_string(),
+    //         tag: "v3.0.0".to_string(),
+    //         ..Default::default()
+    //     },
+    // )
 }
